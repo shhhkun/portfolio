c2cbcfb20caaf6bc78ff8bb98e14e8066e44ca18
Serjo <serjobarron@gmail.com>
2026-05-20 20:16:18 -0700

    assets: added figma and github logos

---
+++ src/app/utils/FigmaLogo.jsx
@@ -0,0 +1,9 @@
+export default function FigmaLogo({ className = "w-5 h-5 mr-2" }) {
+  return (
+    <img
+      src="./Figma-logo.svg"
+      alt="Figma"
+      className={className}
+    />
+  );
+}
---
+++ src/app/utils/GitHubLogo.jsx
@@ -0,0 +1,11 @@
+export default function GitHubLogo({ className = "w-6 h-6 mr-2" }) {
+  const GitHubBlack = "./GitHub_Invertocat_Black.svg";
+  const GitHubWhite = "./GitHub_Invertocat_White.svg";
+  
+  const color = getComputedStyle(document.documentElement)
+    .getPropertyValue("--logo-theme")
+    .trim();
+
+  const logo = color === "dark" ? GitHubBlack : GitHubWhite;
+  return <img src={logo} alt="GitHub" className={className} />;
+}
